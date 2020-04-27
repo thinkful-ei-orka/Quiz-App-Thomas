2f3de5ba596f79c0c76011bfe8adaf6043f7e802
Thomas Davis <ThomasDavisA@gmail.com>
2020-04-26 17:38:50 -0700

    additional code cleaning

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const store = {
   questions: [
     {
@@ -8,7 +10,7 @@ const store = {
         'Lewis',
         'Marnie'
       ],
-      correctAnswer: 'Lewis',
+      correctAnswer: 2,
       incorrectDesc: 'The mayor of Pelican Town is Lewis.',
       factoid: 'Lewis is one of the first to greet you when you move to Stardew Valley!'
     },
@@ -20,9 +22,9 @@ const store = {
         '3 months',
         '4 months'
       ],
-      correctAnswer: '1 month',
+      correctAnswer: 0,
       incorrectDesc: 'Each season is one month long.',
-      factoid: `You can check the calender outside of Pierre's store to keep up with each months events!`
+      factoid: 'You can check the calender outside of Pierre\'s store to keep up with each months events!'
     },
     {
       question: 'Who do you inherit your farm from?',
@@ -32,7 +34,7 @@ const store = {
         'Gus',
         'Elliot'
       ],
-      correctAnswer: 'Your grandfather',
+      correctAnswer: 1,
       incorrectDesc: 'You inherit your farm from your grandfather.',
       factoid: 'You receive it after quitting your job at Joja Corporation!'
     },
@@ -44,7 +46,7 @@ const store = {
         'Gus',
         'Marnie'
       ],
-      correctAnswer: 'Gus',
+      correctAnswer: 2,
       incorrectDesc: 'Gus runs the Stardrop Saloon.',
       factoid: 'Check the arcade for playable games like Junimo Kart!'
     },
@@ -56,7 +58,7 @@ const store = {
         'JojaMart',
         'The Beach'
       ],
-      correctAnswer: 'The Community Center',
+      correctAnswer: 1,
       incorrectDesc: 'You give items to the Junimos at The Community Center.',
       factoid: 'Make sure to complete all bundles to fully restore each room!'
     }
@@ -87,17 +89,18 @@ const store = {
 
 // These functions return HTML templates
 function generateQuizQuestion() {
+  let html = '';
   html = `<section class='js-main-screen'>
             <h2 class='js-question-box'>${store.questions[store.questionNumber].question}</h2>
             <form action='' id='js-answer-form' class='js-answer-box'>`;
   let i = 1;
   store.questions[store.questionNumber].answers.forEach(quizAnswer => {
     html += `<div class='input-container'>
-              <input type="radio" name="answer" id='answer${i}' value='${quizAnswer}'>
+              <input type="radio" required name="answer" id='answer${i}' value='${quizAnswer}'>
               <label for='answer${i}'>${quizAnswer}</label>
             </div>`;
     i++;
-  })
+  });
 
   html += `<section class='js-answer-eval'></section>
            <button type ='submit' class='js-answer-button'>Submit</button>`;
@@ -105,20 +108,21 @@ function generateQuizQuestion() {
   html += `</form>
           </section>`;
   return html;
-};
+}
 
 
 function generateQuizFeedback() {
   let html = '';
   let incorrectFlag = false;
-  let correct = store.questions[store.questionNumber].correctAnswer;
+  let currentQuestion = store.questions[store.questionNumber];
+  let correct = currentQuestion.answers[currentQuestion.correctAnswer];
   html += `<section class='js-main-screen'>
-              <h2 class='js-question-box'>${store.questions[store.questionNumber].question}</h2>
+              <h2 class='js-question-box'>${currentQuestion.question}</h2>
               <form action='' id='js-answer-form' class='js-answer-box'>`
 
   let i = 1;
-  store.questions[store.questionNumber].answers.forEach(quizAnswer => {
-    if (quizAnswer != correct && quizAnswer === store.answer) {
+  currentQuestion.answers.forEach(quizAnswer => {
+    if (quizAnswer !== correct && quizAnswer === store.answer) {
       incorrectFlag = !incorrectFlag;
       html += `<div class='input-container incorrect'>`;
     } else {
@@ -131,16 +135,16 @@ function generateQuizFeedback() {
         html += `<div class='input-container'>`;
       }
     }
-    html += `<input disabled ${store.answer === quizAnswer ? `checked` : ``} type="radio" name="answer" id='answer${i} value='${quizAnswer}'>
+    html += `<input disabled ${store.answer === quizAnswer ? 'checked' : ''} type="radio" name="answer" id='answer${i} value='${quizAnswer}'>
              <label for='answer${i}'>${quizAnswer}</label>
              </div>`;
     i++;
-  })
+  });
   if (incorrectFlag) {
     html += `<section class='js-answer-eval incorrect'>
-               <strong>Incorrect!</strong> ${store.questions[store.questionNumber].incorrectDesc}. Did you know? ${store.questions[store.questionNumber].factoid}</section>`;
+               <strong>Incorrect!</strong> ${currentQuestion.incorrectDesc}. Did you know? ${currentQuestion.factoid}</section>`;
   } else {
-    html += `<section class='js-answer-eval correct'><strong>Correct!</strong> Did you know? ${store.questions[store.questionNumber].factoid}</section>`;
+    html += `<section class='js-answer-eval correct'><strong>Correct!</strong> Did you know? ${currentQuestion.factoid}</section>`;
   }
 
   html += `<button type ='submit' class='js-continue-button'>Continue</button>`;
@@ -154,7 +158,7 @@ function generateQuizFeedback() {
 
 function generateQuizCount() {
   return `<h3>Question ${(store.questionNumber + 1)}/${store.questions.length}. ${store.score} correct.</h3>`;
-};
+}
 
 function generateTitleScreen() {
   return `
@@ -162,7 +166,7 @@ function generateTitleScreen() {
       <h2 class="inner-box center-text">Test your Stardew Valley knowledge with this short quiz!</h2>
       <button type='button' class='js-start-button'>Start!</button>
     </section>`;
-};
+}
 
 function generateEndSceen() {
   return `
@@ -173,8 +177,8 @@ function generateEndSceen() {
         <h3 class='js-results-text'>${Math.floor((store.score / store.questions.length) * 100)}%</h2>
       </div>
         <button class='js-end-button'>Play again!</button>
-    </section>`
-};
+    </section>`;
+}
 
 /********** RENDER FUNCTION(S) **********/
 
@@ -217,14 +221,14 @@ function handleStartQuiz() {
     event.preventDefault();
     store.quizStarted = !store.quizStarted;
     renderQuizScreen();
-  })
-};
+  });
+}
 
 function handleNextQuestion() {
   $('main').on('click', '.js-continue-button', (event) => {
     store.answer = '';
     renderQuizScreen();
-  })
+  });
 }
 
 function handleEndQuiz() {
@@ -233,7 +237,7 @@ function handleEndQuiz() {
     store.questionNumber = 0;
     store.quizStarted = false;
     renderQuizScreen();
-  })
+  });
 }
 
 function handleQuizApp() {
